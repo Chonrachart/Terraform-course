1b5617327d69f2946fbdbfb8d77115e5cfaef506
Chonrachart <chonrachart.sieng@gmail.com>
2026-01-15 17:15:52 +0700

    02-hcl: add first terraform file

---
+++ 02-hcl/hcl.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.37.0"
+    }
+  }
+}
+
+# Actively manage by us, by our Terraform project
+resource "aws_s3_bucket" "mybucket" {
+    bucket = var.bucket_name
+}
+
+# Manage somewhere else. we just want to use in our project
+data "aws_s3_bucket" "my_external_bucket" {
+    bucket = "not-managed-by-us"
+}
+
+variable "bucket_name" {
+    type = string
+    description = "My variable used to set bucket name"
+    default = "my_default_bucket_name"
+}
+
+output "bucket_id" {
+    value = aws_s3_bucket.mybucket.id
+    # value = local.locals_example
+}
+
+locals {
+    locals_example = "This is a local variable"
+}
+
+module "my_module" {
+    source = "./module-example"
+}
